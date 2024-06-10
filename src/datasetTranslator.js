d4ae01e0a0fa4deed9b6a52deee9165c8b7d3c60
StepanChuev <StepanChuev>
2024-06-10 17:29:46 +0400

    The interface has been improved, trained on 60k digits and tested on 10k digits

--- src/datasetTranslator.js
+++ src/datasetTranslator.js
@@ -1,35 +1,3 @@
-'use strict';
-
-const fs = require('node:fs/promises');
-
-const originalFilePath = "../dataset/mnist_test.csv";
-const translatedFilePath = "../dataset/mnist_test_translated.csv";
-
-const main = async () => {
-	const origFile = await fs.open(originalFilePath);
-
-	await fs.writeFile(translatedFilePath, "", err => console.log(err));
-
-	let isFirst = true;
-
-	for await (let line of origFile.readLines()){
-		if (isFirst){
-			isFirst = false;
-			continue;
-		}
-
-		let data = line.split(",");
-
-		for (let i = 1; i < data.length; i++){
-			data[i] = (+data[i]) / 255;
-		}
-
-		line = data.join(",");
-
-		await fs.appendFile(translatedFilePath, line + "\n", (err) => console.log(err));
-	}
-
-	origFile.close();
-};
-
-main();
+version https://git-lfs.github.com/spec/v1
+oid sha256:687f265200e8af84add4c67c877d59d307e2da41b0aa68e468a9a5ce8df1da79
+size 704
