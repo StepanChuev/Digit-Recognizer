491f3f6a8ed7eee759208041c8466bb5681798bf
StepanChuev <133255497+StepanChuev@users.noreply.github.com>
2024-06-12 18:35:02 +0400

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -1,3 +1,65 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ecfa6a4a873dfa34d8d901c606246760579bf13f9a37b80f81b6e98dced439d9
-size 1734
+'use strict';
+
+import Perceptron from './perceptron.mjs';
+import fs from 'node:fs/promises';
+
+const main = async () => {
+	const trainFilePath = "../dataset/mnist_train_translated.csv";
+	const testFilePath = "../dataset/mnist_test_translated.csv";
+	const weightsFilePath = "../dataset/weights.json";
+	const trainFile = await fs.open(trainFilePath);
+	let trainingData = [];
+
+	for await (let line of trainFile.readLines()){
+		let data = line.split(",");
+
+		trainingData.push([+data[0], []]);
+
+		for (let i = 1; i < data.length; i++){
+			trainingData[trainingData.length - 1][1].push(+data[i]);
+		}
+	}
+
+	trainFile.close();
+
+	console.log("File has been read");
+
+	const perceptron = new Perceptron(trainingData, 0.2, 50);
+
+	perceptron.initWeights();
+
+	perceptron.train();
+
+	console.log("Perceptron has been trained");
+
+	trainingData = [];
+	const testFile = await fs.open(testFilePath);
+
+	for await (let line of testFile.readLines()){
+		let data = line.split(",");
+
+		trainingData.push([+data[0], []]);
+
+		for (let i = 1; i < data.length; i++){
+			trainingData[trainingData.length - 1][1].push(+data[i]);
+		}
+	}
+
+	testFile.close();
+
+	let amountRight = 0;
+	let result = 0;
+
+	for (let i = 0; i < trainingData.length; i++){
+		result = perceptron.getResult(trainingData[i][1]);
+		amountRight += (result === trainingData[i][0]);
+	}
+
+	console.log(`Accuracy ${100 * amountRight / trainingData.length}% (${amountRight}/${trainingData.length})`);
+
+
+	await fs.writeFile(weightsFilePath, "{\n\"hiddenWeights\":" + JSON.stringify(perceptron.hiddenWeights) + ",\n", err => console.log(err));
+	await fs.writeFile(weightsFilePath, "\"outputWeights\":" + JSON.stringify(perceptron.outputWeights) + "\n}", { flag: "a" }, err => console.log(err));
+};
+
+main();
